b9c7b27d737eb9bd1d1f6a9b26d5f1942a7e3356
James Han <jameshan20024@gmail.com>
2021-07-12 08:13:10 -0500

    Final fixing minor issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import display from "./modules/display.js";
 const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
+const tempChange = document.getElementById("temperture-change");
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,5 +13,14 @@ searchForm.addEventListener("submit", (e) => {
 searchBtn.addEventListener("click", async () => {
   if (searchInput.value === "") return;
   const weatherData = await weather.cityWeather(searchInput.value);
+  tempChange.checked = false;
+  tempChange.addEventListener("click", () => {
+    if (tempChange.checked === false) {
+      display.setSearchResult(weatherData);
+    } else {
+      display.changeCelcius(weatherData);
+    }
+  });
+
   display.setSearchResult(weatherData);
 });
--- src/modules/display.js
+++ src/modules/display.js
@@ -4,7 +4,9 @@ const display = (() => {
   const cityFeelsLike = document.getElementById("cityFeelsLike");
   const cityHumidity = document.getElementById("cityHumidity");
   const cityWind = document.getElementById("cityWind");
+  const weatherSection = document.getElementById("weather-section");
   function setSearchResult(weatherData) {
+    weatherSection.style.display = "block";
     cityName.textContent = `${weatherData.cityName}`;
     cityTemp.textContent = `${Math.round(weatherData.temperature)}°F`;
     cityFeelsLike.textContent = `Feels like: ${Math.round(
@@ -13,7 +15,14 @@ const display = (() => {
     cityHumidity.textContent = `Humidity: ${weatherData.humidity}%`;
     cityWind.textContent = `Wind: ${weatherData.windSpeed} mph`;
   }
-  return { setSearchResult };
+  function changeCelcius(weatherData) {
+    const fToCelTemp = ((weatherData.temperature - 32) * 5) / 9;
+    const fToCelFeelsLike = ((weatherData.feelsLike - 32) * 5) / 9;
+
+    cityTemp.textContent = `${Math.round(fToCelTemp)}°C`;
+    cityFeelsLike.textContent = `Feels like: ${Math.round(fToCelFeelsLike)}°C`;
+  }
+  return { setSearchResult, changeCelcius };
 })();
 
 export default display;
